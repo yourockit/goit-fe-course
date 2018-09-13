6ec9736d708c666d4fd0465d9c9362eaee9f5162
Yourockit <sts2ro@gmail.com>
2018-09-13 19:08:59 +0300

    js-mod-03-dz

--- java-script/module-03/js/scripts.js
+++ java-script/module-03/js/scripts.js
@@ -5,14 +5,14 @@ const isLoginValid = function(login) {
     if (login.length >= 4 && login.length <= 16) {
         return true;
     } else {
-        // alert('Логин должен состоять от 4 до 16 символов');
+        alert('Ошибка! Логин должен быть от 4 до 16 символов');
         return false;
     }
 }
 
 const isLoginUnique = function(allLogins, login) {
     if (allLogins.includes(login)) {
-        // alert('Такой логин уже есть');
+        alert('Такой логин уже используется!');
         return false;
     } else {
         return true;
@@ -20,16 +20,10 @@ const isLoginUnique = function(allLogins, login) {
 }
 
 const addLogin = function(allLogins, login) {
-    if (!isLoginValid(login)) {
-        alert('Ошибка! Логин должен быть от 4 до 16 символов');
-    } else if (!isLoginUnique(allLogins, login)) {
-        alert('Такой логин уже используется!');
-    } else {
+    if (isLoginValid(login) && isLoginUnique(allLogins, login)) {
         allLogins.push(login);
         alert('Логин успешно добавлен');
     }
-};
-
-// alert(addLogin(allLogins, login));
-console.log(addLogin(allLogins, login));
-// console.log(allLogins);
+}
+addLogin(allLogins, login);
+console.log(allLogins);
