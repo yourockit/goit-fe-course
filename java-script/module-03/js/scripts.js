d9d8176130ccf05b1b813f2194e93521288e58e1
Yourockit <sts2ro@gmail.com>
2018-09-15 11:53:38 +0300

    js-m-03-foo

--- java-script/module-03/js/scripts.js
+++ java-script/module-03/js/scripts.js
@@ -2,25 +2,19 @@ const allLogins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 let login = prompt('Введите Ваш логин');
 
 const isLoginValid = function(login) {
-    if (login.length >= 4 && login.length <= 16) {
-        return true;
-    } else {
-        alert('Ошибка! Логин должен быть от 4 до 16 символов');
-        return false;
-    }
+    return login.length >= 4 && login.length <= 16;
 }
 
 const isLoginUnique = function(allLogins, login) {
-    if (allLogins.includes(login)) {
-        alert('Такой логин уже используется!');
-        return false;
-    } else {
-        return true;
-    }
+    return allLogins.includes(login);
 }
 
 const addLogin = function(allLogins, login) {
-    if (isLoginValid(login) && isLoginUnique(allLogins, login)) {
+    if (!isLoginValid(login)) {
+        alert('Ошибка! Логин должен быть от 4 до 16 символов');
+    } else if (isLoginUnique(allLogins, login)) {
+        alert('Такой логин уже используется!');
+    } else {
         allLogins.push(login);
         alert('Логин успешно добавлен');
     }
