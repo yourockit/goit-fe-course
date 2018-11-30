617732eea6fe1d3f6894c318abdb676176638601
Yourockit <sts2ro@gmail.com>
2018-11-30 23:17:27 +0300

    js-m-08

--- java-script/module-08/js/scripts.js
+++ java-script/module-08/js/scripts.js
@@ -1,85 +1,45 @@
+const gallery = document.querySelector(".image-gallery");
+
 const galleryItems = [
-    { preview: 'img/preview-1.jpeg', fullview: 'img/fullview-1.jpeg', alt: "alt text 1" },
-    { preview: 'img/preview-2.jpeg', fullview: 'img/fullview-2.jpeg', alt: "alt text 2" },
-    { preview: 'img/preview-3.jpeg', fullview: 'img/fullview-3.jpeg', alt: "alt text 3" },
-    { preview: 'img/preview-4.jpeg', fullview: 'img/fullview-4.jpeg', alt: "alt text 4" },
-    { preview: 'img/preview-5.jpeg', fullview: 'img/fullview-5.jpeg', alt: "alt text 5" },
-    { preview: 'img/preview-6.jpeg', fullview: 'img/fullview-6.jpeg', alt: "alt text 6" },
+    { preview: "img/preview-1.jpeg", fullview: "img/fullview-1.jpeg", alt: "alt text 1" },
+    { preview: "img/preview-2.jpeg", fullview: "img/fullview-2.jpeg", alt: "alt text 2" },
+    { preview: "img/preview-3.jpeg", fullview: "img/fullview-3.jpeg", alt: "alt text 3" },
+    { preview: "img/preview-4.jpeg", fullview: "img/fullview-4.jpeg", alt: "alt text 4" },
+    { preview: "img/preview-5.jpeg", fullview: "img/fullview-5.jpeg", alt: "alt text 5" },
+    { preview: "img/preview-6.jpeg", fullview: "img/fullview-6.jpeg", alt: "alt text 6" },
 ];
 
-function createGallery(itemsGallery) {
-    const imageGallery = document.querySelector(".js-image-gallery");
-    const fullview = document.createElement("div");
-    fullview.classList.add("fullview");
-    const fullviewElement = document.createElement("img");
-    fullviewElement.setAttribute("src", itemsGallery[0].fullview);
-    fullviewElement.setAttribute("alt", itemsGallery[0].alt);
-    fullview.appendChild(fullviewElement);
-    const preview = document.createElement("ul");
-    preview.classList.add("preview");
-    imageGallery.append(fullview, preview);
+const fullView = document.createElement("div");
+fullView.classList.add("fullview");
+fullView.innerHTML = '<img src="img/fullview-1.jpeg" alt="alt text 1">';
+const preView = document.createElement("ul");
+preView.classList.add("preview");
 
-    itemsGallery.forEach(elem => {
-        const img = createLiImgEl(elem);
-        img.addEventListener("click", clickListener);
-    });
-
-    function clickListener(event) {
-        const dataAtr = event.target.getAttribute("data-fullview");
-        const altAtr = event.target.getAttribute("alt");
-        fullview.firstElementChild.setAttribute("src", dataAtr);
-        fullview.firstElementChild.setAttribute("alt", altAtr);
-
-        const target = event.target;
-
-        setActiveLink(target);
-    }
-
-    function setActiveLink(nextTarget) {
-        const currentTarget = preview.querySelector("img.active");
-
-        if (currentTarget) {
-            currentTarget.classList.remove("active");
-        }
-
-        nextTarget.classList.add("active");
-    }
-
-    // function createLiImgEl(elem) {
-    //     const liElement = document.createElement("li");
-    //     // preview.append(liElement);
-    //     const imgElement = document.createElement("img");
-    //     liElement.appendChild(imgElement).setAttribute("src", elem.preview);
-    //     imgElement.setAttribute("data-fullview", elem.fullview);
-    //     imgElement.setAttribute("alt", elem.alt);
-    //     if (imgElement.getAttribute("alt") === "alt text 1") {
-    //         imgElement.classList.add("active");
-    //     }
-    //     // imgArray.push(imgElement);
-    //     return imgElement;
-    // }
-
-    function createLiImgEl(elem) {
-        const liElement = document.createElement("li");
-        const imgElement = document.createElement("img");
-        imgElement.setAttribute("src", elem.preview);
-        imgElement.setAttribute("data-fullview", elem.fullview);
-        imgElement.setAttribute("alt", elem.alt);
-        liElement.append(imgElement);
-        if (imgElement.getAttribute("alt") === "alt text 1") {
-            imgElement.classList.add("active");
-        }
-        return liElement;
-    }
-
-    const createElem = galleryItems => galleryItems.map(item => createLiImgEl(item));
-
+for (const item of galleryItems) {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.setAttribute("src", item.preview);
+    img.setAttribute("data-fullview", item.fullview);
+    img.setAttribute("alt", item.alt);
+    li.append(img);
+    preView.append(li);
 }
 
+gallery.append(fullView, preView);
 
-createGallery(galleryItems);
+let currentSelectedImage = preView.firstElementChild;
 
+const selectImgPreview = event => {
+    if (currentSelectedImage.classList.contains("active")) {
+        currentSelectedImage.classList.remove("active");
+    }
+    const targetImage = event.target;
+    fullviewImage.firstElementChild.setAttribute("src", targetImage.dataset.fullview);
+    targetImage.classList.add("active");
+    currentSelectedImage = targetImage;
+}
 
-https: //github.com/cursorksu/goit-fe-advanced/blob/master/module_8/hometask/js/script.js
+const previewList = document.querySelector(".preview");
+const fullviewImage = document.querySelector(".fullview");
 
-    https: //github.com/SergioKhodchenko/JavaScript/blob/master/module_08/script.js
+previewList.addEventListener("click", selectImgPreview);
